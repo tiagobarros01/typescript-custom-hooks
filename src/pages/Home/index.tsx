277b5f9c9e7c77cfc5e99f528a8d1d11cc8cec2d
Tiago Barros <tbarros1337@hotmail.com>
2021-11-12 14:21:44 -0300

    feat: creates a useNumber

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
+import { useNumber } from '../../hooks/useNumber';
 import './index.css';
 
 export const Home = (): JSX.Element => {
+  const [count, setCount] = useNumber(20);
+
   return (
-    <div className="container">
-      <h1 className="title">Hello world</h1>
+    <div>
+      <h1>Hello world</h1>
+
+      <h2>{count}</h2>
+
+      <button type="button" onClick={() => setCount((prevState) => ++prevState)} >Counter</button>
     </div>
   );
 };
