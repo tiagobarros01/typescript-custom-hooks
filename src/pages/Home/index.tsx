7edb7c896d1adb058799f4229acf643d5bdeaafa
Tiago Barros <tbarros1337@hotmail.com>
2021-11-12 14:27:35 -0300

    feat: creates a useIsMounted

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+import { useIsMounted } from '../../hooks/useIsMounted';
 import { useNumber } from '../../hooks/useNumber';
 import './index.css';
 
 export const Home = (): JSX.Element => {
   const [count, setCount] = useNumber(20);
+  const isMounted = useIsMounted();
+
+  useEffect(() => {
+    if (isMounted) {
+      console.log('Rendered component')
+    }
+  }, [isMounted])
 
   return (
     <div>
